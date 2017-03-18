8cd1d3b00df1063803b5e7a0e3f9ea77194e4af8
Sergii Zagryvyi <serzhcello@gmail.com>
2017-03-18 19:37:23 +0200

    add RestService & Dao

---
+++ src/main/java/org/forge/demo/function/TempInterface.java
@@ -0,0 +1,7 @@
+package org.forge.demo.model.function;
+
+/**
+ * Created by Serzh on 3/18/17.
+ */
+public class TempInterface {
+}
---
+++ src/main/java/org/forge/demo/model/Product.java
@@ -0,0 +1,95 @@
+package org.forge.demo.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "products")
+@XmlRootElement
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "price")
+	private double price;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Product)) {
+			return false;
+		}
+		Product other = (Product) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (id != null)
+			result += "id: " + id;
+		result += ", version: " + version;
+		if (name != null && !name.trim().isEmpty())
+			result += ", name: " + name;
+		result += ", price: " + price;
+		return result;
+	}
+}
